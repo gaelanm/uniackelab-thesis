b3e539719c86f1f7aaf294c8d2cb08a6c5b66050
gaelanm <44978773+gaelanm@users.noreply.github.com>
2021-02-04 14:39:44 -0500

    Create get_gene_list.py

---
+++ gProfiler/get_gene_list.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+from datetime import datetime
+
+
+def generate_gene_list(in_path, name, out_path):
+    """
+    Return g:Profiler-compatible gene list from excel file.
+
+    :param str in_path: Input excel file path
+    :param str name: Desired output file name
+    :param str out_path: Output path
+    """
+
+    df = pd.read_excel(f'{in_path}', header=None, index_col=None)
+    date = datetime.today().strftime('%d-%m-%Y')
+
+    return df.to_csv(f'{out_path}/{name}_{date}.txt', sep='\n', header=False, index=False)
+
